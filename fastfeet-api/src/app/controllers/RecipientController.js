f858e6dbe95adacc207f7868dec801f698e55b4d
Gustavo Souza <gustavo.ifto@gmail.com>
2020-02-20 22:37:07 -0300

    Inserindo as Pastas do Projeto

---
+++ fastfeet-api/src/app/controllers/RecipientController.js
@@ -0,0 +1,93 @@
+import * as Yup from 'yup';
+
+import Recipient from '../models/Recipient';
+
+class RecipientController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      street: Yup.string().required(),
+      street_number: Yup.number().required(),
+      complement: Yup.string(),
+      state: Yup.string().required(),
+      city: Yup.string().required(),
+      zipcode: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { name } = req.body;
+    const recipient = await Recipient.findOne({ where: { name } });
+    if (recipient) {
+      return res.json({ error: 'Recipient already exists' });
+    }
+
+    const {
+      street,
+      street_number,
+      complement,
+      state,
+      city,
+      zipcode,
+    } = await Recipient.create(req.body);
+
+    return res.json({
+      name,
+      adrress: {
+        street,
+        street_number,
+        complement,
+        state,
+        city,
+        zipcode,
+      },
+    });
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      street: Yup.string(),
+      street_number: Yup.number(),
+      complement: Yup.string(),
+      state: Yup.string(),
+      city: Yup.string(),
+      zipcode: Yup.string(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { name } = req.body;
+    const recipient = await Recipient.findOne({ where: { name } });
+    if (!recipient) {
+      return res.json({ error: 'Recipient not found' });
+    }
+
+    const {
+      street,
+      street_number,
+      complement,
+      state,
+      city,
+      zipcode,
+    } = await recipient.update(req.body);
+
+    return res.json({
+      name,
+      adrress: {
+        street,
+        street_number,
+        complement,
+        state,
+        city,
+        zipcode,
+      },
+    });
+  }
+}
+
+export default new RecipientController();
